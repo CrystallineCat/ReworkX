239724abb510f706555890eb5371d6c47962639d
M. Niederlechner <mniederlechner@gmx.net>
2018-12-03 20:51:02 +0100

    Switched from pyparsing to parglare and added first parsing test.

--- tests/test_projx.py
+++ tests/test_projx.py
@@ -1,18 +1,35 @@
 import projx as px
 
 
-def try_to_transform(source: str, etl: str, target: str) -> None:
-    graph  = px.load_graph(f'examples/{source}.graph.yaml')
-    expect = px.load_graph(f'examples/{target}.graph.yaml')
-    via    = px.load_etl  (f'examples/{etl}.etl.yaml')
-    result = px.execute_etl(via, graph)
+def test_execute_etl() -> None:
 
-    assert px.content_of(result)['nodes'] == px.content_of(expect)['nodes']
-    assert px.content_of(result)['edges'] == px.content_of(expect)['edges']
+    def try_to_transform(source: str, etl: str, target: str) -> None:
+        graph = px.load_graph(f'examples/{source}.graph.yaml')
+        expect = px.load_graph(f'examples/{target}.graph.yaml')
+        via = px.load_etl(f'examples/{etl}.etl.yaml')
+        result = px.execute_etl(via, graph)
 
+        assert px.content_of(result)['nodes'] == px.content_of(expect)['nodes']
+        assert px.content_of(result)['edges'] == px.content_of(expect)['edges']
 
-def test_apply_etl() -> None:
     try_to_transform('example', 'project',         'projected')
     try_to_transform('example', 'transfer',        'transferred')
     try_to_transform('example', 'combine',         'combined')
     try_to_transform('example', 'multi_transform', 'multi_transformed')
+
+
+def test_parse_query() -> None:
+
+    def try_to_parse(etl: str, query: str) -> None:
+        expect = px.load_etl(f'examples/{etl}.etl.yaml')
+        result = px.parse_query(query)
+
+        assert result == expect
+
+    try_to_parse('project', '''
+        MATCH   (p1:Person)-(wild)-(p2:Person)
+        PROJECT (p1)-(p2)
+        METHOD JACCARD Institution, City
+        SET     name = wild.label
+        DELETE  wild
+    ''')
