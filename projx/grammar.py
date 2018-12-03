ef9b0164d02cf4e25727a3172a2441fbffc0a9d7
M. Niederlechner <mniederlechner@gmx.net>
2018-12-03 21:29:31 +0100

    Some grammar improvements.

--- projx/grammar.py
+++ projx/grammar.py
@@ -1,33 +1,40 @@
 import parglare as g
 
 
+def merge_dicts(*dicts):
+    d = {}
+
+    for x in dicts:
+        d.update(x)
+
+    return d
+
+
+def collect_flat(_, nodes):
+    return nodes[0] + nodes[1:] if len(nodes) > 1 else nodes or []
+
+
+grammar = g.Grammar.from_file('projx/grammar.pg')
+parser  = g.Parser(grammar, actions={
+    'collect_flat': collect_flat,
+    'make_dict':  lambda context, nodes, **kwds: kwds,
+    'key_value':  lambda context, nodes: {nodes[0].lower(): nodes[1]},
+    'join_dicts': lambda context, nodes, **kwds: merge_dicts(*[node for node in nodes if isinstance(node, dict)], kwds),
+})
+
+
 def parse_query(query: str) -> dict:
-    def merge_dicts(*dicts):
-        d = {}
+    data = parser.parse(query)
 
-        for x in dicts:
-            d.update(x)
-
-        return d
-
-    prsd = g.Parser(g.Grammar.from_file('projx/grammar.pg'), actions={
-        'make_dict':  lambda context, nodes, **kwds: kwds,
-        'pass_lower': lambda context, nodes: (nodes[0] or '').lower(),
-        'key_value':  lambda context, nodes: {nodes[0].lower(): nodes[1]},
-        'join_dicts': lambda context, nodes: merge_dicts(*nodes),
-    }).parse(query)
-
-    etl = {
+    return {
         'extractor': {
             'networkx': {
-                'type': prsd['type'] or 'subgraph',
-                'traversal': prsd['traversal'],
-            }
+                'type':      (data['type'] or 'subgraph').lower(),
+                'traversal': data['traversal'],
+            },
         },
-        'transformers': prsd['transformers'],
+        'transformers': data['transformers'],
         'loader': {
             'nx2nx': {},
-        }
+        },
     }
-    
-    return etl
